4b88154b0a3154b843e3440b08bb1ac77f0114b9
Martin Metodiev <metodiev.martin@gmail.com>
2018-07-29 14:16:36 +0300

    Footer styles

--- client/src/app/components/organisms/footer/footer.js
+++ client/src/app/components/organisms/footer/footer.js
@@ -10,11 +10,16 @@ import React from 'react';
 // Styles
 import './footer.scss';
 
+// Atoms
+import Hyperlink from '../../atoms/hyperlink';
+
 
 //--------------------------| Component
 
 const Footer = props => (
-  <footer className='po-footer' />
+  <footer className='po-footer'>
+    <Hyperlink>Your best shots</Hyperlink>
+  </footer>
 );
 
 
