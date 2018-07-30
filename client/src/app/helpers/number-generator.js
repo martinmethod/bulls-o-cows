9758ac225ae5d313a94a4eabcb140c5a003fa9af
Martin Metodiev <metodiev.martin@gmail.com>
2018-07-30 18:22:25 +0300

    Number generator

--- client/src/app/helpers/number-generator.js
+++ client/src/app/helpers/number-generator.js
@@ -2,4 +2,22 @@
 // NUMBER GENERATOR
 
 
-export default () => '1234'; // TODO: Write down the code that generates the number
+export default () => {
+  const nums = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+  const ranNums = [];
+  let i = nums.length;
+  let num = 0;
+
+  const getRandomNum = () => {
+    num = Math.floor(Math.random() * (i + 1));
+    if (i === 9 && num === 0) getRandomNum();
+  };
+
+  while (i--) { // eslint-disable-line no-plusplus
+    getRandomNum();
+    ranNums.push(nums[num]);
+    nums.splice(num, 1);
+  }
+
+  return ranNums.splice(0, 4).join('');
+};
